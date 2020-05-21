304c9fcdb2a56f0f0b9e08345bab0a63bc9e795e
martin-trnecka <42838144+martin-trnecka@users.noreply.github.com>
2020-05-21 19:55:49 +0200

    novy model vyhodnocovani
    predelano na zasobnikovy model vyhodnocovani

--- blocky/alan_generators.js
+++ blocky/alan_generators.js
@@ -1,40 +1,45 @@
 Blockly.JavaScript['krok'] = function(block) {
-  var code = 'hra.proved("hra.krok()");\n';
+  //var code = 'hra.proved("hra.krok()");\n';
+  var code = 'KROK\n';
   return code;
 };
 
 Blockly.JavaScript['opakuj'] = function(block) {
   var number_i = block.getFieldValue('i');
   var statements_name = Blockly.JavaScript.statementToCode(block, 'NAME');
+  var code = '';
 
-  var code = '{let i='+number_i+';\n';
-  code += 'while(i > 0) {\n';
+  var code = 'OPAKUJ ' + number_i +' \n';
   code += statements_name;
-  code += 'i--;}}\n';
+  code += 'END\n';
+
   return code;
 };
 
 Blockly.JavaScript['opakuj_p1'] = function(block) {
   var statements_name = Blockly.JavaScript.statementToCode(block, 'NAME');
+  var code = '';
 
-  var code = '{let i=5;\n';
-  code += 'while(hra.podminka_1() && i>0) {\n';
+  var code = 'WHILE P1 \n';
   code += statements_name;
-  code += 'i--;}}\n';
+  code += 'END\n';
   return code;
 };
 
 Blockly.JavaScript['seber_slepici'] = function(block) {
-  var code = 'hra.proved("hra.seber_slepici()");\n';
+  //var code = 'hra.proved("hra.seber_slepici()");\n';
+  var code = 'SEBER_SLEPICI\n';
   return code;
 };
 
 Blockly.JavaScript['otoc_vlevo'] = function(block) {
-  var code = 'hra.proved("hra.otoc_vlevo()");\n';
+  //var code = 'hra.proved("hra.otoc_vlevo()");\n';
+  var code = 'OTOC_VLEVO\n';
   return code;
 };
 
 Blockly.JavaScript['otoc_vpravo'] = function(block) {
-  var code = 'hra.proved("hra.otoc_vpravo()");\n';
+  //var code = 'hra.proved("hra.otoc_vpravo()");\n';
+  var code = 'OTOC_VPRAVO\n';
   return code;
 };
